6a39ffaa997250f5add921b656a22ab72c8f630e
jose-1805 <joseluiscapotemosquera@gmail.com>
2022-11-11 14:40:51 -0500

    Migración de base de datos #2

--- database/migrations/2022_11_11_141801_create_counters_table.php
+++ database/migrations/2022_11_11_141801_create_counters_table.php
@@ -16,8 +16,8 @@ return new class () extends Migration {
             $table->uuid('id')->unique()->comment('Identificador único de cada registro');
             $table->float('amount')->default(0)->comment('Cantidad actual asignada por el contador');
             $table->dateTime('date')->nullable()->comment('Fecha asociada al contador (sirve para agrupar datos de un mismo contador por fecha)');
-            $table->string('item_type', 45)->nullable()->comment('Nombre del modelo o entidad propietario del archivo');
-            $table->string('item', 45)->nullable()->comment('Identificador del elemento propietario del archivo');
+            $table->string('item_type', 45)->nullable()->comment('Nombre del modelo o entidad asociado al registro');
+            $table->string('item', 45)->nullable()->comment('Identificador del elemento asociado al registro');
             $table->string('extra_name', 45)->nullable()->comment('Nombre de un campo extra para almacenar y asociar al archivo');
             $table->string('extra_data', 250)->nullable()->comment('Valor del campo extra para almacenar y asociar al archivo');
             $table->timestamps();
